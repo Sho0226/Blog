d71852a467a88998dc2fcabae90dd5f7240ae2fb
sho0226 <s1f102201616@iniad.org>
2024-11-28 17:41:12 +0900

    add: GET res/req

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export async function main() {
+  try {
+  } catch (err) {}
+}
+
+export const get = async (req: Request, res: NextResponse) => {
+  try {
+  } catch (err) {
+  } finally {
+  }
+};
