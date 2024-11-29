35967b6bfe89b5b81e581f0e9271eb570daa62a1
sho0226 <s1f102201616@iniad.org>
2024-11-29 15:48:01 +0900

    add: GET [id]

---
+++ src/app/api/[id]/route.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { main } from "../blog/route";
+
+const prisma = new PrismaClient();
+
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+    await main();
+    const post = await prisma.post.findFirst({ where: { id } });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
